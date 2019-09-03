222e17b7731c5d2807d50dbabeeb37a7c4ff5aab
Ian Bastos <ian-bastos@live.com>
2019-09-03 02:05:58 +0100

    mouse release add fish handler

--- src/main/java/com/tropicalbastos/boids/core/Renderer.java
+++ src/main/java/com/tropicalbastos/boids/core/Renderer.java
@@ -4,13 +4,17 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import com.tropicalbastos.boids.objects.Fish;
 
-public class Renderer extends Canvas {
+public class Renderer extends Canvas implements MouseListener {
 
     public Renderer() {
         setBackground(new Color(0x006994));
+        addMouseListener(this);
     }
 
     public void pack() {
@@ -27,5 +31,33 @@ public class Renderer extends Canvas {
             f.draw(g);
         }
     }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        Simulation sim = Simulation.getInstance();
+        Point point = e.getPoint();
+        Fish f = new Fish((int) point.getX(),(int) point.getY(), this);
+        sim.addFish(f);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+	}
     
 }
