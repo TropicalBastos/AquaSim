30eb3d41fb413707798c7c98b6c65833d644e624
Sam <samkeshmiri94@gmail.com>
2019-10-10 22:54:24 +0100

    updated formatting to reflect proper indentation

--- src/main/java/com/tropicalbastos/boids/settings/Platform.java
+++ src/main/java/com/tropicalbastos/boids/settings/Platform.java
@@ -1,15 +1,16 @@
 package com.tropicalbastos.boids.settings;
 
 public final class Platform {
-   private static String OS = null;
-   public static String getOsName() {
-      if(OS == null) { 
-         OS = System.getProperty("os.name"); 
-      }
-      return OS;
-   }
-   
-   public static boolean isMac() {
-		return (getOsName().toLowerCase().indexOf("mac") >= 0);
-	}
+    private static String OS = null;
+
+    public static String getOsName() {
+        if (OS == null) {
+            OS = System.getProperty("os.name");
+        }
+        return OS;
+    }
+
+    public static boolean isMac() {
+        return (getOsName().toLowerCase().indexOf("mac") >= 0);
+    }
 }
